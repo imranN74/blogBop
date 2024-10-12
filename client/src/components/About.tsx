653193c8edf83963f4fdc1ebf56de3a883c62d28
imranN74 <imrannaziransari69@gmail.com>
2024-10-13 04:48:04 +0530

    added about page

---
+++ client/src/components/About.tsx
@@ -0,0 +1,14 @@
+export const About = () => {
+  return (
+    <div>
+      <div className="text-xl">About blogBop</div>
+      <div>
+        "BlogBop" is a platform where anyone can post blogs and users can read
+        through a variety of content. Whether you want to share your thoughts or
+        explore what others are writing, blogBop makes it easy for everyone to
+        participate. Users can create posts, browse through different blogs, and
+        engage with content from across the community.
+      </div>
+    </div>
+  );
+};
---
+++ client/src/pages/AboutPage.tsx
@@ -0,0 +1,11 @@
+import { About } from "../components/About";
+
+export const AboutPage = () => {
+  return (
+    <div className="grif grid-cols-5">
+      <div className="col-start-2 col-end-5">
+        <About />
+      </div>
+    </div>
+  );
+};
