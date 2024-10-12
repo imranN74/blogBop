7a957f4312d2aeed40c0c4facf6da221fa845c5c
imranN74 <imrannaziransari69@gmail.com>
2024-10-13 04:34:08 +0530

    solving hamburger toggle issue in mobile

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,10 +3,23 @@ import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { authAtom } from "../store/atom";
 import { LogButton } from "./Logbutton";
+import { useState, useEffect } from "react";
 
 export const Navbar = () => {
   const navigate = useNavigate();
   const isLoggedIn = useRecoilValue(authAtom);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen); // Toggle the menu state
+  };
+
+  useEffect(() => {
+    const button = document.getElementById("menu-toggle-button");
+    if (button) {
+      button.setAttribute("aria-expanded", menuOpen ? "true" : "false"); // Set dynamically using JavaScript
+    }
+  }, [menuOpen]);
 
   return (
     <nav className="cursor-pointer bg-white fixed top-0 left-0 right-0 z-50 border-gray-200 dark:bg-gray-900 border-b dark:border-gray-700">
@@ -20,9 +33,10 @@ export const Navbar = () => {
         <button
           data-collapse-toggle="navbar-multi-level"
           type="button"
+          onClick={toggleMenu}
           className="cursor-pointer inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-multi-level"
-          aria-expanded="false"
+          id="menu-toggle-button"
         >
           <span className="cursor-pointer sr-only">Open main menu</span>
           <svg
@@ -42,7 +56,9 @@ export const Navbar = () => {
           </svg>
         </button>
         <div
-          className="cursor-pointer hidden w-full md:block md:w-auto"
+          className={`cursor-pointer w-full md:block md:w-auto ${
+            menuOpen ? "" : "hidden"
+          }`}
           id="navbar-multi-level"
         >
           <ul className="cursor-pointer flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
