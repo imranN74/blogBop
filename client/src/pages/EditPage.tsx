653971f5a2792d93c08073289d9892fa80ff34c3
imranN74 <imrannaziransari69@gmail.com>
2024-11-05 12:51:54 +0530

    add change in edit form layout

--- client/src/pages/EditPage.tsx
+++ client/src/pages/EditPage.tsx
@@ -3,7 +3,7 @@ import { CreateBlogForm } from "../components/CreateBlogForm";
 export const EditPage = () => {
   return (
     <div className="grid grid-cols-10">
-      <div className="col-start-3 col-end-9">
+      <div className="col-span-full md:col-start-3 md:col-end-9">
         <CreateBlogForm isEdit={true} />
       </div>
     </div>
